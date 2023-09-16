76d7a7aab2ad1c7cef1b8b93aeb4558b68281172
Jatin <imjattin@gmail.com>
2023-09-16 10:48:04 +0530

    flake8

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,7 @@
 Calculator functions
 """
 
+
 def add(x, y):
     """Add x and y"""
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,13 +2,14 @@
 Sample tests
 """
 
-from django.test import SimpleTestCase
 
+from django.test import SimpleTestCase
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """Test the calc module """
-    
+
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 7)
