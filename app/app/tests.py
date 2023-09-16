d2b84af303c070d1c76d7e708c21e3f245521af7
Jatin Yadav <imjatinofficial@gmail.com>
2023-09-16 18:07:09 +0530

    checks to wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,7 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module """
+    """Test the calc module"""
 
     def test_add_numbers(self):
         """Test adding numbers together"""
