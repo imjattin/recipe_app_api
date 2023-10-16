078fea64206a6cf9d30d389031377841e7ea58bb
Jatin <imjattin@gmail.com>
2023-10-16 15:48:50 +0530

    Build recipe API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -49,16 +49,15 @@ class ModelTests(TestCase):
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
         user = get_user_model().objects.create_user(
-            'test@example.com',
-            'testpass123',
+            "test@example.com",
+            "testpass123",
         )
         recipe = models.Recipe.objects.create(
             user=user,
-            title='Sampel recipe name',
+            title="Sampel recipe name",
             time_minutes=5,
-            price=Decimal('5.50'),
-            description='Sample recipe description.',
+            price=Decimal("5.50"),
+            description="Sample recipe description.",
         )
 
         self.assertEqual(str(recipe), recipe.title)
-        
